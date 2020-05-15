b26e92401516cf63400c3a2507664e506fa8d5ed
xuwuhui <wuhui.xu@newtouch.cn>
2020-05-15 15:32:07 +0800

    nio select channel 示例

---
+++ java-io/src/main/java/com/demo/io/NioClient.java
@@ -0,0 +1,132 @@
+package com.demo.io;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * <p><b>Description:</b>
+ * TODO
+ * <p><b>Company:</b>
+ *
+ * @author created by Jesse Hsu at 17:10 on 2020/5/14
+ * @version V0.1
+ * @classNmae NioClient
+ */
+public class NioClient {
+
+    Selector selector;
+
+    SocketChannel client;
+
+    ByteBuffer receivebuffer = ByteBuffer.allocate(1024);
+
+    public static void main(String[] args) throws IOException {
+        NioClient client = new NioClient();
+        client.initClient(8090);
+        client.listener();
+    }
+
+    public void initClient(int port) throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        // 设置为非阻塞方式
+        socketChannel.configureBlocking(false);
+        // 打开选择器
+        selector = Selector.open();
+        // 注册连接服务端socket动作
+        socketChannel.register(selector, SelectionKey.OP_CONNECT);
+        socketChannel.connect(new InetSocketAddress(8090));
+        System.out.println("准备连接服务器");
+    }
+
+    public void listener() throws IOException {
+        System.out.println("开始监听服务端数据");
+        SelectionKey selectionKey = null;
+        while (true) {
+            selector.select();
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            //依次处理事件
+            while (iterator.hasNext()) {
+                selectionKey = iterator.next();
+                if (selectionKey.isConnectable()) {
+                    doConnect(selectionKey);
+                }
+                if (selectionKey.isAcceptable()) {
+                    doAccept(selectionKey);
+                }
+                if (selectionKey.isReadable()) {
+                    doRead(selectionKey);
+                }
+                if (selectionKey.isWritable()) {
+                    doWrite(selectionKey);
+                }
+            }
+        }
+
+    }
+
+    private void doConnect(SelectionKey selectionKey) throws IOException {
+
+        System.out.println("client connect");
+
+        client = (SocketChannel) selectionKey.channel();
+        // 判断此通道上是否正在进行连接操作。
+        // 完成套接字通道的连接过程。
+        receivebuffer.clear();
+        if (client.isConnectionPending()) {
+            client.finishConnect();
+            int len = client.read(receivebuffer);
+            while(len>0){
+                String  receiveText = new String(receivebuffer.array(), 0, len);
+                System.out.println("from server --:" + receiveText);
+                len = client.read(receivebuffer);
+            }
+            System.out.println("server connected!");
+            client.write(ByteBuffer.wrap(new String("Hello Server!").getBytes("UTF-8")));
+        }
+        client.register(selector, SelectionKey.OP_READ);
+    }
+
+    private void doAccept(SelectionKey selectionKey) throws IOException {
+        System.out.println("client doAccept");
+    }
+
+    private void doRead(SelectionKey selectionKey) throws IOException {
+        SocketChannel server = (SocketChannel) selectionKey.channel();
+        ByteBuffer receivebuffer = ByteBuffer.allocate(1024);
+        // 返回为之创建此键的通道。
+        server = (SocketChannel) selectionKey.channel();
+        //将缓冲区清空以备下次读取
+        receivebuffer.clear();
+        //读取服务器发送来的数据到缓冲区中
+        StringBuilder sb =  new StringBuilder();
+        int count = server.read(receivebuffer);
+        while (count > 0) {
+            String  receiveText = new String(receivebuffer.array(), 0, count);
+            sb.append(receiveText);
+            count = server.read(receivebuffer);
+        }
+        System.out.println("from server:" + sb.toString());
+        server.register(selector, SelectionKey.OP_WRITE);
+    }
+
+    private void doWrite(SelectionKey selectionKey) throws IOException {
+        //写数据给client
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("client send:");
+        String message =scanner.nextLine();
+        client = (SocketChannel) selectionKey.channel();
+        client.write(ByteBuffer.wrap(new String(message).getBytes("UTF-8")));
+        //注册selector
+        client.register(selectionKey.selector(), SelectionKey.OP_READ);
+    }
+}
---
+++ java-io/src/main/java/com/demo/io/NioServer.java
@@ -0,0 +1,152 @@
+package com.demo.io;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * <p><b>Description:</b>
+ * TODO
+ * <p><b>Company:</b>
+ *
+ * @author created by Jesse Hsu at 16:52 on 2020/5/14
+ * @version V0.1
+ * @classNmae NioServer
+ */
+public class NioServer {
+
+    private Selector selector;
+
+    private StringBuilder message = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        NioServer server = new NioServer();
+        server.initServer(8090);
+        server.listener();
+    }
+
+    private void acceptRequest(SelectionKey selectionKey) throws IOException {
+        //获取channel
+        SocketChannel socketChannel = ((ServerSocketChannel) selectionKey.channel()).accept();
+        //非阻塞
+        socketChannel.configureBlocking(false);
+        //注册selector
+        socketChannel.register(selectionKey.selector(), SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+
+        System.out.println("建立请求......");
+
+    }
+
+    private String doRequest(StringBuilder req) {
+
+
+        System.out.println("开始处理请求 request = " + req);
+
+        String response = req.reverse().toString();
+
+        System.out.println("处理请求结束");
+        return response;
+    }
+
+    private void initServer(int port) throws IOException {
+
+        // 获得一个ServerSocketChannel通道
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        // 设置通道为非阻塞
+        serverSocketChannel.configureBlocking(false);
+        // 将该通道对应的ServerSocket绑定到port端口
+        ServerSocket serverSocket = serverSocketChannel.socket();
+        serverSocket.bind(new InetSocketAddress(port));
+
+        selector = Selector.open();
+
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+
+    }
+
+
+    public void listener() {
+        System.out.println("服务端启动成功");
+        try {
+            while (true) {
+                selector.select();  //如果没有事件，则一致阻塞
+                Iterator<SelectionKey> keyIterator = selector.selectedKeys().iterator();
+                //依次处理事件
+                while (keyIterator.hasNext()) {
+
+                    SelectionKey key = keyIterator.next();
+
+                    if (key.isConnectable()) {
+                        doConnect(key);
+                    }
+                    // 客户端请求连接事件
+                    if (key.isAcceptable()) {
+                        doAccept(key);
+                    }
+                    if (key.isReadable()) {
+                        doRead(key);
+                    }
+                    if (key.isWritable()) {
+                        doWrite(key);
+                    }
+                    //处理完后移除当前使用的key
+                    keyIterator.remove();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doWrite(SelectionKey selectionKey) throws IOException {
+        //写数据给client
+
+        SocketChannel client = (SocketChannel) selectionKey.channel();
+        client.write(ByteBuffer.wrap(new String(message.toString()).getBytes("UTF-8")));
+        //注册selector
+        client.register(selectionKey.selector(), SelectionKey.OP_READ);
+    }
+
+    private void doRead(SelectionKey selectionKey) throws IOException {
+        SocketChannel client = (SocketChannel) selectionKey.channel();
+        ByteBuffer receivebuffer = ByteBuffer.allocate(1024);
+        // 返回为之创建此键的通道。
+        client = (SocketChannel) selectionKey.channel();
+        //将缓冲区清空以备下次读取
+        receivebuffer.clear();
+        //读取服务器发送来的数据到缓冲区中
+        message.setLength(0);
+        int count = client.read(receivebuffer);
+        if (count > 0) {
+            String receiveText = new String(receivebuffer.array(), 0, count);
+            message.append(receiveText);
+
+        }
+        System.out.println("from client:" + message.toString());
+        client.register(selector, SelectionKey.OP_WRITE);
+    }
+
+    private void doConnect(SelectionKey selectionKey) {
+        System.out.println("server doConnect");
+    }
+
+    private void doAccept(SelectionKey selectionKey) throws IOException {
+
+        SocketChannel socketChannel = ((ServerSocketChannel) selectionKey.channel()).accept();
+        //非阻塞
+        socketChannel.configureBlocking(false);
+
+        socketChannel.write(ByteBuffer.wrap(new String("连接成功！").getBytes("UTF-8")));
+
+        //注册selector
+        socketChannel.register(selectionKey.selector(), SelectionKey.OP_READ);
+        System.out.println("客户端" + socketChannel.getLocalAddress() + "链接成功！");
+    }
+}
