59e1f079774d938ada159d0de53d2ed2eb118bd6
xuwuhui <wuhui.xu@newtouch.cn>
2020-05-28 10:34:59 +0800

    Calculator 运行卡死修复

--- java-thread/src/main/java/com/demo/thread/Calculator.java
+++ java-thread/src/main/java/com/demo/thread/Calculator.java
@@ -15,6 +15,7 @@ public class Calculator implements Runnable {
             if(isPrimes(current)){
                 numPrimes++;
             }
+            current++;
         }
         System.out.printf("Thread '%s': END. Number of primes: %d\n",Thread.currentThread().getName(),numPrimes);
     }
@@ -35,10 +36,10 @@ public class Calculator implements Runnable {
         System.out.printf("Minimum Priority: %s\n",Thread.MIN_PRIORITY);
         System.out.printf("Normal Priority: %s\n",Thread.NORM_PRIORITY);
         System.out.printf("Maximum Priority: %s\n",Thread.MAX_PRIORITY);
-
-        Thread[] threads = new Thread[10];
-        Thread.State states[] =new Thread.State[10];
-        for (int i=0;i<10;i++){
+        int count =10;
+        Thread[] threads = new Thread[count];
+        Thread.State states[] =new Thread.State[count];
+        for (int i=0;i<count;i++){
             threads[i] = new Thread(new Calculator());
             if((i%2)==0){
                 threads[i].setPriority(Thread.MAX_PRIORITY);
@@ -49,23 +50,23 @@ public class Calculator implements Runnable {
         }
         try(FileWriter fileWriter = new FileWriter("log.txt")) {
             PrintWriter pw = new PrintWriter(fileWriter);
-            for(int i =0;i<10;i++){
+            for(int i =0;i<count;i++){
                 pw.println("Main : Status of Thread "+i+":"+threads[i].getState());
                 states[i] = threads[i].getState();
             }
-            for(int i =0;i<10;i++){
+            for(int i =0;i<count;i++){
                threads[i].start();
             }
             boolean finish = false;
             while (!finish){
-                for(int i =0;i<10;i++){
+                for(int i =0;i<count;i++){
                    if(threads[i].getState()!=states[i]){
                        writeThreadInfo(pw,threads[i],states[i]);
                        states[i] = threads[i].getState();
                    }
                 }
                 finish = true;
-                for(int i =0;i<10;i++){
+                for(int i =0;i<count;i++){
                    finish = finish&& (threads[i].getState()== Thread.State.TERMINATED);
                 }
             }
