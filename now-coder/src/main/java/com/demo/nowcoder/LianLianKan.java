52b0bfbc010c5efe2c0eed07b572172d424b6feb
xwh9004 <xwh9004@163.com>
2021-02-01 23:03:19 +0800

    newcoder project

--- now-coder/src/main/java/com/demo/nowcoder/LianLianKan.java
+++ now-coder/src/main/java/com/demo/nowcoder/LianLianKan.java
@@ -1,20 +1,23 @@
 package com.demo.nowcoder;
+/**
+ * 连连看，消除连续重复字符 aaebbecd  --> cd;
+ */
 
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Main1 {
+public class LianLianKan {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String input =  scanner.nextLine();
-            System.out.println(cleanRepeate(input));
+            System.out.println(cleanRepeat(input));
         }
 
     }
 
-    private static int cleanRepeate(String str) {
+    private static int cleanRepeat(String str) {
         Stack<Character> stacks = new Stack<>();
         for (int i =0;i<str.length();i++) {
             if ((str.charAt(i) <= 'A' && str.charAt(i) >= 'a')
---
+++ now-coder/src/main/java/com/demo/nowcoder/RemoveLessString.java
@@ -0,0 +1,49 @@
+package com.demo.nowcoder;
+
+import java.util.*;
+
+/**
+ * 实现删除字符串中出现次数最少的字符，若多个字符出现次数一样，则都删除。输出删除这些单词后的字符串，字符串中其它字符保持原来的顺序。
+ * 注意每个输入文件有多组输入，即多个字符串用回车隔开
+ */
+public class RemoveLessString {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String input =  scanner.next();
+            System.out.println(removeLessString(input));
+        }
+
+    }
+
+    private static String removeLessString(String str) {
+        Map<Character,Integer> map = new HashMap();
+
+        for(int i=0;i<str.length();i++){
+           char k = str.charAt(i);
+           if(map.containsKey(k)){
+               int count = map.get(k);
+               map.put(k,count+1) ;
+           }else{
+                map.put(k,1) ;
+            }
+        }
+
+        Set<Character> kSet = map.keySet();
+        int min = 1;
+        for (char k: kSet){
+            min = Math.min(min,map.get(k));
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (int i=0;i<str.length();i++){
+           char k = str.charAt(i);
+           if(map.get(k)!=min){
+               sb.append(k);
+           }
+       }
+        return sb.toString();
+
+    }
+}
