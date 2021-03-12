ca7444593b7db96c7b66730371ac2bc9439389cd
xuwuhui <wuhui.xu@newtouch.cn>
2021-03-12 10:23:28 +0800

    查找链表中间节点

--- leet-code/src/main/java/com/demo/leetcode/ListNodeSolution.java
+++ leet-code/src/main/java/com/demo/leetcode/ListNodeSolution.java
@@ -1,6 +1,7 @@
 package com.demo.leetcode;
 
 
+import lombok.val;
 import org.junit.Test;
 
 public class ListNodeSolution {
@@ -14,10 +15,10 @@ public class ListNodeSolution {
   public void printListNode(ListNode head){
      ListNode node = head;
      while (node!=null){
-         System.out.print(node.val +"——>");
+         System.out.print(node.val +" ");
          node = node.next;
      }
-      System.out.println("null");
+      System.out.println();
   }
 
   @Test
@@ -31,10 +32,77 @@ public class ListNodeSolution {
       nd_3.next = nd_4;
       ListNode nd_5 = new ListNode(5);
       nd_4.next = nd_5;
+      ListNode nd_6 = new ListNode(6);
+//      nd_5.next = nd_6;
+//      nd_6.next = nd_4;
       printListNode(head);
-      head = reverseList1(head);
-      printListNode(head);
+      printListNode(indexMiddle(head));
 
+  }
+
+
+  //合并两个有序链表
+
+  //删除链表倒数第n个节点
+    //求链表的中间节点
+
+
+    public ListNode indexMiddle(ListNode head){
+        ListNode slowPoint = head;
+        ListNode fastPoint = head;
+        boolean isOdd = false; //是否是奇数个节点
+
+        //slowPoint 步长为1；fastPoint 步长为2；
+        while (fastPoint!=null){
+
+            fastPoint =fastPoint.next;
+            if(fastPoint==null){
+                //reach the link end
+                 isOdd = true;
+                 break;
+            }
+            fastPoint =fastPoint.next;
+            if(fastPoint==null){
+                //reach the link end
+                isOdd = false;
+                break;
+            }
+            slowPoint =slowPoint.next;
+        }
+        if(isOdd){
+            return new ListNode(slowPoint.val);
+        }else{
+            ListNode node1  = new ListNode(slowPoint.val);
+            ListNode node2  = new ListNode(slowPoint.next.val);
+            node1.next = node2;
+            return node1;
+        }
+
+    }
+
+    /**
+     * 链表中环检测
+     * @param head
+     * @return
+     */
+  public boolean circleTest(ListNode head){
+      ListNode p1 = head;
+      ListNode p2 = head;
+
+      //p1 步长为1；p2步长为2；
+      while (p1!=null && p2!=null){
+          p2 =p2.next;
+          if(p2==null){
+              //reach the link end
+            return false;
+          }
+          p2 =p2.next;
+          p1 =p1.next;
+          if(p1 == p2){
+              return true;
+          }
+      }
+      return false;
   }
     /**
      * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点
