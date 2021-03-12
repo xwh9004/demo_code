7b2bd3c9b75b78eed3a7c35650e6e690636ee819
xuwuhui <wuhui.xu@newtouch.cn>
2021-03-12 15:42:16 +0800

    查找链表是否有环,链表环入口

--- leet-code/src/main/java/com/demo/leetcode/ListNodeSolution.java
+++ leet-code/src/main/java/com/demo/leetcode/ListNodeSolution.java
@@ -2,6 +2,7 @@ package com.demo.leetcode;
 
 
 import lombok.val;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ListNodeSolution {
@@ -21,9 +22,10 @@ public class ListNodeSolution {
       System.out.println();
   }
 
-  @Test
-  public void test(){
-      ListNode head = new ListNode(1);
+  public  ListNode head = null;
+  @Before
+  public void headNode(){
+       head = new ListNode(1);
       ListNode nd_2 = new ListNode(2);
       head.next = nd_2;
       ListNode nd_3 = new ListNode(3);
@@ -33,20 +35,102 @@ public class ListNodeSolution {
       ListNode nd_5 = new ListNode(5);
       nd_4.next = nd_5;
       ListNode nd_6 = new ListNode(6);
-//      nd_5.next = nd_6;
-//      nd_6.next = nd_4;
-      printListNode(head);
-      printListNode(indexMiddle(head));
+      nd_5.next = nd_6;
+      nd_6.next = nd_2;
+
+//      printListNode(indexMiddle(head));
+//      System.out.println(taiIndexOf(head,1).val);
+//      System.out.println(taiIndexOf(head,2).val);
+//      System.out.println(taiIndexOf(head,3).val);
+//      System.out.println(taiIndexOf(head,4).val);
+//      System.out.println(taiIndexOf(head,5).val);
+//      System.out.println(taiIndexOf(head,6).val);
+//      printListNode(removeIndexOf(head,2));
+//      printListNode(removeIndexOf(head,2));
+//      printListNode(removeIndexOf(head,1));
+//      printListNode(removeIndexOf(head,1));
+//      printListNode(removeIndexOf(head,1));
+//      printListNode(removeIndexOf(head,1));
+  }
+  @Test
+  public void testPrint(){
+      if(!circleTest(head)){
+          printListNode(head);
+      }else{
+          System.out.println("list is contains one circle");
+      }
 
   }
 
+    @Test
+    public void testNodeCircle(){
+        System.out.println(circleStartAt(head).val);
+    }
 
-  //合并两个有序链表
 
-  //删除链表倒数第n个节点
+  //删除倒数第n个节点
+  public ListNode removeIndexOf(ListNode head,int n){
+      if(n<1){
+          throw new IllegalArgumentException("n must be greater than 1");
+      }
+      ListNode prevNode = head;
+      ListNode current_p = head;
+      ListNode nextNode = head;
+      int goSteps = n-1;
+      while (nextNode!=null && goSteps>0){
+          nextNode = nextNode.next;
+          goSteps--;
+      }
+      if(goSteps>0||nextNode==null){
+          throw new IndexOutOfBoundsException("n can not be greater than link size");
+      }
+      //nextNode move forward step than currentNode;
+      nextNode = nextNode.next;
+      //nextNode and current_p move forward one step than prevNode
+      if(nextNode ==null){
+          head = head.next;
+          return head;
+      }
+      nextNode = nextNode.next;
+      current_p = current_p.next;
+      //nextNode,current_p,prevNode move forward together
+      while (nextNode!=null){
+          nextNode = nextNode.next;
+          current_p = current_p.next;
+          prevNode = prevNode.next;
+      }
+      //
+      prevNode.next = current_p.next;
+      return head;
+  }
+    /**
+     *  查找链表倒数第n个节点
+     *  n=1时,指向的链表中最后一个
+     */
+
+    public ListNode taiIndexOf(ListNode head,int n){
+      if(n<1){
+          throw new IllegalArgumentException("n must be greater than 1");
+      }
+
+      ListNode bp = head;
+      ListNode fp = head;
+      int goSteps = n-1;
+      while (fp!=null && goSteps>0){
+          fp = fp.next;
+          goSteps--;
+      }
+      if(goSteps>0||fp==null){
+          throw new IndexOutOfBoundsException("n can not be greater than link size");
+      }
+      fp = fp.next;
+      while (fp!=null){
+          fp = fp.next;
+          bp = bp.next;
+      }
+      return bp;
+    }
     //求链表的中间节点
-
-
     public ListNode indexMiddle(ListNode head){
         ListNode slowPoint = head;
         ListNode fastPoint = head;
@@ -80,29 +164,49 @@ public class ListNodeSolution {
 
     }
 
+    public ListNode circleStartAt(ListNode head){
+        ListNode start = head;
+        ListNode atNode =circleEncounterAt(head);
+        if(atNode!=null){
+            ListNode circleRunNode = atNode;
+            while (start!=null && start!=circleRunNode){
+                start=start.next;
+                circleRunNode =circleRunNode.next;
+
+            }
+            return circleRunNode;
+        }
+        return null;
+    }
+
+    public ListNode circleEncounterAt(ListNode head){
+        ListNode p1 = head;
+        ListNode p2 = head;
+
+        //p1 步长为1；p2步长为2；
+        while (p1!=null && p2!=null){
+            p2 =p2.next;
+            if(p2==null){
+                //reach the link end
+                return null;
+            }
+            p2 =p2.next;
+            p1 =p1.next;
+            if(p1 == p2){
+                return p1;
+            }
+        }
+        return null;
+    }
+
     /**
      * 链表中环检测
      * @param head
      * @return
      */
   public boolean circleTest(ListNode head){
-      ListNode p1 = head;
-      ListNode p2 = head;
-
-      //p1 步长为1；p2步长为2；
-      while (p1!=null && p2!=null){
-          p2 =p2.next;
-          if(p2==null){
-              //reach the link end
-            return false;
-          }
-          p2 =p2.next;
-          p1 =p1.next;
-          if(p1 == p2){
-              return true;
-          }
-      }
-      return false;
+      ListNode encounterAt = circleEncounterAt(head);
+      return encounterAt!=null;
   }
     /**
      * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点
