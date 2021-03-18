3dc8036a2f72a3ababb3394420e3a8aaf1be0be2
xuwuhui <wuhui.xu@newtouch.cn>
2021-03-18 16:18:51 +0800

    基数排序

--- sort-util/src/main/java/com/demo/sort/util/SortUtil.java
+++ sort-util/src/main/java/com/demo/sort/util/SortUtil.java
@@ -15,11 +15,22 @@ public class SortUtil {
         }
 
 // int[] array1 = {74 ,11 ,55 ,50 ,71 ,64 ,47, 46, 40 ,76};
-        int[] array1 = {11,8,9,3,7,1,2,5};
-        System.out.print("数组：");
+//        int[] array1 = {11,8,9,3,7,1,2,5};
+//        System.out.print("数组：");
+//        printArray(array1);
+//        System.out.println(SortUtil.findMaxN(array1,1));
+//        System.out.println(SortUtil.findMaxN(array1,2));
+//        System.out.println(SortUtil.findMaxN(array1,3));
+//        System.out.println(SortUtil.findMaxN(array1,4));
+//        System.out.println(SortUtil.findMaxN(array1,5));
+//        System.out.println(SortUtil.findMaxN(array1,6));
+//        System.out.println(SortUtil.findMaxN(array1,7));
+//        System.out.println(SortUtil.findMaxN(array1,8));
+//        SortUtil.quick_sort(array1,0,array1.length-1);
+//        System.out.println(SortUtil.isSorted(array1));
+        int[] array1 = {2,5,3,0,2,3,0,3};
+        SortUtil.countingSort1(array1,array1.length);
         printArray(array1);
-        SortUtil.quick_sort(array1,0,array1.length-1);
-        System.out.println(SortUtil.isSorted(array1));
     }
 
 
@@ -39,21 +50,17 @@ public class SortUtil {
     public static int partition(int[] array,int low,int high){
         int k = array[low];
         int pl = low;
-        int ph = high;
+        int ph = high +1;
         while (true){
-            while (pl<high && array[pl]<=k){
-                pl++;
-            }
-            while (ph>low&& array[ph]>k){
-                ph--;
-            }
+            while (array[++pl]<=k)if(pl==high)break;
+            while (array[--ph]>k)if(ph==low)break;
             if(pl>=ph){
                 break;
             }
             swap(array,pl,ph);
         }
         swap(array,low,ph);
-        return pl;
+        return ph;
 
     }
     public static void merge_sort(int[] array,int low,int high){
@@ -166,4 +173,99 @@ public class SortUtil {
         }
         System.out.println();
     }
+
+    public static int findMaxN(int[] arr,int n){
+        int len = arr.length ;
+        //第n大的元素一定是在排序好的len-n-1 处
+        int index = len -n;
+        int i = partition(arr,0,len-1);
+        while (index!=i){
+            if(i>index){
+                i = partition(arr,0,i-1);
+            }else{
+                i = partition(arr,i+1,index);
+            }
+        }
+
+        return arr[i];
+    }
+
+    //计数排序，a是数组，n是数组大小
+    public static void countingSort1(int[] a,int n){
+        if(n<=1){
+            return;
+        }
+        //查找数组中数据的返回
+        int max =a[0];
+        for (int i =1;i<n;++i){
+            if(max<a[i]){
+                max = a[i];
+            }
+        }
+        int[] c = new int[max+1];
+        for (int i=0;i<=max;++i){
+            c[i] = 0;
+        }
+        //计算每个元素的个数，放入c中
+        for (int i=0;i<n;i++){
+            c[a[i]]++;
+        }
+        //一次累加
+        for (int i=1;i<=max;i++){
+            c[i] = c[i-1]+c[i];
+        }
+        //临时数组r,存储排序之后的结果
+        int[] r = new int[n];
+        int prev =0;
+        for (int i=0;i<=max;i++){
+           int value = i;
+           int count = c[i];
+           for (int j=count-1;j>=prev;j--){
+               r[j]=value;
+           }
+           prev =count;
+        }
+        //将结果拷贝给  a数组
+        for (int i=0;i<n;i++){
+            a[i]=r[i];
+        }
+    }
+
+    //计数排序，a是数组，n是数组大小
+    public static void countingSort(int[] a,int n){
+        if(n<=1){
+            return;
+        }
+        //查找数组中数据的返回
+        int max =a[0];
+        for (int i =1;i<n;++i){
+            if(max<a[i]){
+                max = a[i];
+            }
+        }
+        int[] c = new int[max+1];
+        for (int i=0;i<=max;++i){
+            c[i] = 0;
+        }
+        //计算每个元素的个数，放入c中
+        for (int i=0;i<n;i++){
+            c[a[i]]++;
+        }
+        //一次累加
+        for (int i=1;i<=max;i++){
+            c[i] = c[i-1]+c[i];
+        }
+        //临时数组r,存储排序之后的结果
+        int[] r = new int[n];
+        for (int i=n-1;i>=0;--i){
+            int index =c[a[i]] -1;
+            r[index] =a[i];
+            c[a[i]]--;
+        }
+        //将结果拷贝给  a数组
+        for (int i=0;i<n;i++){
+            a[i]=r[i];
+        }
+    }
+
 }
