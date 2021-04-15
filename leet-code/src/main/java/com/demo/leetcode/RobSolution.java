acecb2f5e6b4a827d69c5c489a6162b2deffaf6c
xwh9004 <xwh9004@163.com>
2021-04-15 22:13:27 +0800

    打家劫舍1 动态规划解法2

--- leet-code/src/main/java/com/demo/leetcode/RobSolution.java
+++ leet-code/src/main/java/com/demo/leetcode/RobSolution.java
@@ -4,8 +4,10 @@ public class RobSolution {
 
     public static void main(String[] args) {
         RobSolution solution = new RobSolution();
-        int[] nums ={1,2};
-        System.out.println(solution.rob(nums));
+//        int[] nums ={1,2};
+//        int[] nums ={1};
+        int[] nums ={1,2,9,1,1,10,6};
+        System.out.println(solution.rob1(nums));
     }
 
     public int rob(int[] nums){
@@ -22,5 +24,23 @@ public class RobSolution {
             robMax[i] = Math.max(robMax[i-1],robMax[i-2]+nums[i]);
         }
        return robMax[nums.length -1];
+    }
+     public int rob1(int[] nums){
+        if(nums.length==0){
+            return 0;
+        }
+        if(nums.length==1){
+            return nums[0];
+        }
+        //前一个最大
+        int first_max = nums[0];
+        //前两个最大
+        int second_max= 0 ;
+        for (int i=1;i< nums.length;i++) {
+            int tmp_max = Math.max(first_max,second_max+nums[i]);
+            second_max= first_max ;
+            first_max = tmp_max;
+        }
+        return first_max;
     }
 }
