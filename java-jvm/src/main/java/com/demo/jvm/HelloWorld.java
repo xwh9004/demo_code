06cc357cf37ee0dd102e27a02f1a28335f47020c
xwh9004 <xwh9004@163.com>
2021-07-11 17:37:12 +0800

    HelloWorld

---
+++ java-jvm/src/main/java/com/demo/jvm/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.demo.jvm;
+
+public class HelloWorld {
+
+    public static void foo(){
+        int a =0;
+        int b=6;
+        int c =130;
+        return;
+//        int d =3300;
+    }
+
+}
