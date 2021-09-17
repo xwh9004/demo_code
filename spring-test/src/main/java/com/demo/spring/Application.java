dd1082568d4a0bf3edd97771803613b6782d9084
xwh9004 <xwh9004@163.com>
2021-09-17 14:37:50 +0800

    spring 循环依赖测试

--- spring-test/src/main/java/com/demo/spring/Application.java
+++ spring-test/src/main/java/com/demo/spring/Application.java
@@ -1,8 +1,10 @@
-package com.example.spring;
+package com.demo.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @SpringBootApplication
 public class Application {
 
--- spring-test/src/main/java/com/demo/spring/beans/ClassB.java
+++ spring-test/src/main/java/com/demo/spring/beans/ClassB.java
@@ -1,4 +1,4 @@
-package com.demo.springtest.beans;
+package com.demo.spring.beans;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
