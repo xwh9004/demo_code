dbfa6498fbfa544c993435118cde9579d567514d
Jesse <xwh9004@163.com>
2022-06-23 12:18:13 +0800

    es demo

--- elasticsearch-test/src/test/java/com/demo/elasticsearch/ESIndexQueryTest.java
+++ elasticsearch-test/src/test/java/com/demo/elasticsearch/ESIndexQueryTest.java
@@ -1,8 +1,13 @@
 package com.demo.elasticsearch;
 
 import com.demo.elasticsearch.bean.Movies;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.QueryStringQueryBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,11 +15,9 @@ import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.MultiGetItem;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
-import org.springframework.data.elasticsearch.core.query.Criteria;
-import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.Query;
+import org.springframework.data.elasticsearch.core.query.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -26,10 +29,13 @@ public class ESIndexQueryTest {
     @Autowired
     private ElasticsearchOperations elasticsearchOperations;
 
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
+
     @Test
     public void findById() {
         IndexCoordinates index = IndexCoordinates.of("movies");
-        Movies movie = elasticsearchOperations.get("wJCtlHsB1chFk4J-A8hm",Movies.class,index);
+        Movies movie = elasticsearchOperations.get("wJCtlHsB1chFk4J-A8hm", Movies.class, index);
         System.out.println(movie);
     }
 
@@ -44,16 +50,18 @@ public class ESIndexQueryTest {
         List<MultiGetItem<Movies>> movieList = elasticsearchOperations.multiGet(nativeSearchQuery.build(), Movies.class, index);
         System.out.println(movieList.size());
     }
+
     @Test
     public void findByFieldValues() {
 
         IndexCoordinates index = IndexCoordinates.of("movies");
         NativeSearchQueryBuilder nativeSearchQuery = new NativeSearchQueryBuilder();
-        QueryBuilder queryBuilder = QueryBuilders.termsQuery("movieId","1","2","3");
+        QueryBuilder queryBuilder = QueryBuilders.termsQuery("movieId", "1", "2", "3");
         nativeSearchQuery.withFilter(queryBuilder);
         SearchHits<Movies> movieList = elasticsearchOperations.search(nativeSearchQuery.build(), Movies.class, index);
         System.out.println(movieList.stream().count());
     }
+
     @Test
     public void findByField() {
         IndexCoordinates index = IndexCoordinates.of("movies");
@@ -68,12 +76,37 @@ public class ESIndexQueryTest {
     @Test
     public void matchPhraseWithSlop() {
         IndexCoordinates index = IndexCoordinates.of("movies");
-        QueryBuilder queryBuilder = QueryBuilders.matchPhraseQuery("title","City the").slop(4);
+        QueryBuilder queryBuilder = QueryBuilders.matchPhraseQuery("title", "City the").slop(4);
         NativeSearchQueryBuilder nativeSearchQuery = new NativeSearchQueryBuilder();
         nativeSearchQuery.withQuery(queryBuilder);
         SearchHits<Movies> hints = elasticsearchOperations.search(nativeSearchQuery.build(), Movies.class, index);
         System.out.println(hints.stream().count());
         hints.forEach(System.out::println);
+
+    }
+
+    @Test
+    public void findByStringQuery() {
+        IndexCoordinates index = IndexCoordinates.of("movies");
+        String source = "{\n" +
+                "    \"term\": {\n" +
+                "      \"title\": {\n" +
+                "        \"value\": \"2012\"\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }";
+        StringQuery stringQuery = new StringQuery(source);
+        SearchHits<Movies> hints = elasticsearchOperations.search(stringQuery, Movies.class, index);
+        System.out.println(hints.stream().count());
+        hints.forEach(System.out::println);
+    }
+
+    @Test
+    public void queryByParams() throws IOException {
+        SearchRequest request = new SearchRequest();
+        request.indices("movies");
+
+        restHighLevelClient.search(request, RequestOptions.DEFAULT);
 
     }
 }
