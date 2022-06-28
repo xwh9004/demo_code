2f5d597927b4461d4afce9cee83a1e8695045ebc
Jesse <xwh9004@163.com>
2022-06-28 17:09:50 +0800

    need auth

--- spring-test/src/main/java/com/demo/spring/controller/HelloController.java
+++ spring-test/src/main/java/com/demo/spring/controller/HelloController.java
@@ -2,10 +2,15 @@ package com.demo.spring.controller;
 
 
 import com.demo.excel.ExcelUtil;
+import org.apache.coyote.Response;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.http.HttpResponse;
 
 
 @RestController
@@ -23,5 +28,11 @@ public class HelloController {
     @RequestMapping(path = "ok", method = RequestMethod.GET)
     public String hi() {
         return "ok";
+    }
+
+    @RequestMapping(path = "needAuth", method = RequestMethod.GET)
+    public void needAuth(HttpServletRequest request, HttpServletResponse response) {
+        response.setStatus(401);
+        response.addHeader("WWW-Authenticate","Basic realm=Realm");
     }
 }
