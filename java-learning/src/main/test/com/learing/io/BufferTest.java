bf8cc5869aff7706c1ecd1118b49f9c79f404435
Jesse <xwh9004@163.com>
2023-02-21 10:21:48 +0800

    intBuffer test

---
+++ java-learning/src/main/test/com/learing/io/BufferTest.java
@@ -0,0 +1,66 @@
+package com.learing.io;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.IntBuffer;
+
+/**
+ * @author: xwh90
+ * @date: 2023/2/21 9:49
+ * @description:
+ */
+@Slf4j
+public class BufferTest {
+
+    static IntBuffer intBuffer = null;
+
+    @Before
+    public void createTest() {
+
+        intBuffer = IntBuffer.allocate(20);
+        log.info("------ after allocate -------");
+        printArgs();
+
+    }
+
+    private void printArgs() {
+        log.info("position =" + intBuffer.position());
+        log.info("limit =" + intBuffer.limit());
+        log.info("capacity =" + intBuffer.capacity());
+    }
+
+
+    @Test
+    public void putTest() {
+
+        log.info("------ after put -------");
+
+        for (int i = 0; i < 5; i++) {
+            intBuffer.put(i);
+        }
+        printArgs();
+    }
+
+
+    @Test
+    public void readTest() {
+        flipTest();  //不能直接put后读取
+
+        log.info("------ after read -------");
+
+        for (int i = 0; i < 3; i++) {
+            log.info("get index[{}] ={}", i, intBuffer.get());
+        }
+        printArgs();
+    }
+
+    @Test
+    public void flipTest() {
+        putTest();
+        intBuffer.flip();
+        log.info("------ after flip -------");
+        printArgs();
+    }
+}
