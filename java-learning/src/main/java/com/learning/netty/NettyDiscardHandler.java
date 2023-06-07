ae04087923bfa209a85ff53f31e593da7487a1d6
Jesse <xwh9004@163.com>
2023-06-07 17:20:42 +0800

    netty discard demo

---
+++ java-learning/src/main/java/com/learning/netty/NettyDiscardHandler.java
@@ -0,0 +1,32 @@
+package com.learning.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author: xwh90
+ * @date: 2023/6/6 16:22
+ * @description:
+ */
+public class NettyDiscardHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("接收消息，丢弃如下：");
+        try {
+            while (in.isReadable()) {
+                System.out.print((char) in.readByte());
+            }
+            System.out.println();
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+}
