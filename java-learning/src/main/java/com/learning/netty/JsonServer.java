a62be3a3b40baba0934b2c95588a2bcc9695604e
Jesse <xwh9004@163.com>
2023-07-17 16:22:29 +0800

    发送json数据包

--- java-learning/src/main/java/com/learning/netty/JsonServer.java
+++ java-learning/src/main/java/com/learning/netty/JsonServer.java
@@ -1,10 +1,10 @@
 package com.learning.netty;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.learning.pojo.User;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -16,27 +16,25 @@ import java.nio.charset.StandardCharsets;
 
 /**
  * @author: xwh90
- * @date: 2023/6/6 16:11
- * @description: netty demo
+ * @date: 2023/7/17 15:58
+ * @description:
  */
 @Slf4j
-public class NettyEchoServer {
-
+public class JsonServer {
     private final int serverPort;
 
-    private ServerBootstrap bootstrap = new ServerBootstrap();
-
-    public NettyEchoServer(int port) {
+    public JsonServer(int port) {
         this.serverPort = port;
     }
 
     public void runServer() {
-        //创建反应器轮询组
         EventLoopGroup bossLoopGroup = new NioEventLoopGroup(1);
 
         EventLoopGroup workLoopGroup = new NioEventLoopGroup();
 
         try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+
             //1 设置反应器轮询组
             bootstrap.group(bossLoopGroup, workLoopGroup);
             //设置nio类型的通道
@@ -46,28 +44,24 @@ public class NettyEchoServer {
             //设置通道参数
             bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
 
-            LengthFieldBasedFrameDecoder lengthFieldBasedFrameDecoder = new LengthFieldBasedFrameDecoder(1024,0,4,0,4);
-
-            //装配子通道流水线
             bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
                 @Override
-                protected void initChannel(SocketChannel socketChannel) throws Exception {
-                    //流水线的职责：负责管理通道中的处理器
-                    socketChannel.pipeline().addLast(lengthFieldBasedFrameDecoder);
-                    socketChannel.pipeline().addLast(new StringDecoder(StandardCharsets.UTF_8));
-                    socketChannel.pipeline().addLast(new StringProcessHandler());
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new LengthFieldBasedFrameDecoder(1024, 0, 4, 0, 4));
+                    ch.pipeline().addLast(new StringDecoder(StandardCharsets.UTF_8));
+                    ch.pipeline().addLast(new JsonMsgDecoder());
                 }
             });
-
             final ChannelFuture channelFuture = bootstrap.bind().sync();
             log.info("服务器启动成功，监听端口:{}", channelFuture.channel().localAddress());
             //等待通道关闭的异步任务结束，服务监听通道会一直等待通道关闭的异步任务结束
             final ChannelFuture closeFuture = channelFuture.channel().closeFuture();
 
             closeFuture.sync();
+
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             //8 优雅关闭EventLoopGroup
             workLoopGroup.shutdownGracefully();
 
@@ -76,6 +70,19 @@ public class NettyEchoServer {
     }
 
     public static void main(String[] args) {
-        new NettyEchoServer(18899).runServer();
+        new JsonServer(18899).runServer();
     }
+
+    static class JsonMsgDecoder extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            String json = (String) msg;
+
+            final User user = JSON.parseObject(json, User.class);
+
+            log.info("收到一个json数据包 =>> {}", user);
+            super.channelRead(ctx, msg);
+        }
+    }
+
 }
